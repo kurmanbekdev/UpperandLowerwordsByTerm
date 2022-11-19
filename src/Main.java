cd9709f351a555f0453eeff7103e1c64ced5c669
07elbekdev <masrapovelbek24@gmail.com>
2022-11-19 17:14:09 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+      Class classes = new Class(123,"Hello my friend",new int[]{34567});
+        System.out.println(classes);
+    }
+}
