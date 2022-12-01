f88f6540bca5c41cc8e031eb0bf19133886483be
07elbekdev <masrapovelbek24@gmail.com>
2022-12-02 00:28:49 +0600

    I added a lot of things here.

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);//here every word will be different like ElBeK;
         System.out.println("Please enter a word you want : ");
-        String name = in.nextLine();
-        char[] chars = name.toCharArray();
+        word(in.nextLine());
+    }
+    public static void word(String a){
+        char[] chars = a.toCharArray();
         for (int i = 0; i < chars.length; i++) {
             if (i % 2 == 0) {
                 System.out.print(String.valueOf(chars[i]).toLowerCase());
