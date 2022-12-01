d818eb760b7c340e65a9d1acc8977e0c2cc0aad1
07elbekdev <masrapovelbek24@gmail.com>
2022-12-02 00:26:27 +0600

    hhjjhg

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-      Class classes = new Class(123,"Hello my friend",new int[]{34567});
-        System.out.println(classes);
+        Scanner in = new Scanner(System.in);//here every word will be different like ElBeK;
+        System.out.println("Please enter a word you want : ");
+        String name = in.nextLine();
+        char[] chars = name.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (i % 2 == 0) {
+                System.out.print(String.valueOf(chars[i]).toLowerCase());
+            } else {
+                System.out.print(String.valueOf(chars[i]).toUpperCase());
+            }
+        }
     }
 }
